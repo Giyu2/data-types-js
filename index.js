7d6c21e857c23fca6bc55dbc90710a65c90c7fca
Giyu2 <derdene719@gmail.com>
2024-12-02 03:08:14 -0800

    dasgal

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello world from JS");
+// Data Types //
+// 1.String => Text type => " text "
+// 2.Number => number type =>1 6 1000
+// 3.Boolean => true or false type => false / true
+// 4.Object => { key:value }
+// 5.Array => [ index,index ]
+// 6.Null Or Undefined => no value
